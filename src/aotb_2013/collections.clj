69758dd44580fa22360ad6e771cd3082648a2238
Jim Barritt <jim@planet-ix.com>
2013-08-30 13:49:23 +0100

    [jmdb] Some filtering

--- src/aotb_2013/collections.clj
+++ src/aotb_2013/collections.clj
@@ -2,11 +2,37 @@
 
 (def integers (range))
 
-(take 5 integers)
+(take 20 integers)
 
-(sort integers) ;; Sort them descending
+(sort (take 20 integers)) ;; Sort them descending
 
-;; Filter out all odd numbers
+;; Extract only odd numbers
+(filter odd? (take 20 integers))
 
 ;; Filter out all odd numbers divisible by three
+(filter (fn [x] (and (odd? x)
+                    (= 3 x)))
+        (take 20 integers))
+
+
+;; Filter a map based on some function:
+(def customers [
+  {:is [:customer] :name "Fred B"  :age 23 :height [256 :cm]}
+  {:is [:customer] :name "Joan C"  :age 56 :height [280 :cm]}
+  {:is [:customer] :name "Billy F" :age 17 :height [270 :cm]}
+  {:is [:customer] :name "Jenny J" :age 31 :height [260 :cm]}
+])
+
+;; Filter out customers 
+(defn between [x lower upper]
+  (and (> x lower)
+       (< x upper)))
+
+(defn select-customer [customer]
+  (let [{:keys [age height]} customer]
+       (and (between age 15 30)
+            (between (first height) 200 270))))
+
+
+(filter select-customer customers)
 
--- src/aotb_2013/hello_world.clj
+++ src/aotb_2013/hello_world.clj
@@ -14,7 +14,7 @@
 
 (time (sleep-for 1000))
 
-(spit "/Users/jmdb/Desktop/jims-file.txt" "Hey this is a real easy way to create file!")
+(spit "/Users/jmdb/Desktop/jims-file.txt" "Hey this is a really easy way to create file!")
 
 (slurp "/Users/jmdb/Desktop/jims-file.txt")
 
