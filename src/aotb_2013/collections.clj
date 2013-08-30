47dc94d2466617e3c8eb1153659d117a7f5d5786
Jim Barritt <jim@planet-ix.com>
2013-08-30 14:00:25 +0100

    [jmdb] Tidying up the filtering and making it parameterisable

--- src/aotb_2013/collections.clj
+++ src/aotb_2013/collections.clj
@@ -17,22 +17,28 @@
 
 ;; Filter a map based on some function:
 (def customers [
-  {:is [:customer] :name "Fred B"  :age 23 :height [256 :cm]}
-  {:is [:customer] :name "Joan C"  :age 56 :height [280 :cm]}
   {:is [:customer] :name "Billy F" :age 17 :height [270 :cm]}
+  {:is [:customer] :name "Fred B"  :age 23 :height [256 :cm]}
   {:is [:customer] :name "Jenny J" :age 31 :height [260 :cm]}
+  {:is [:customer] :name "Joan C"  :age 56 :height [280 :cm]}  
 ])
 
+
 ;; Filter out customers 
-(defn between [x lower upper]
-  (and (> x lower)
-       (< x upper)))
+(defn between [x range]
+  (let [[min max] range]
+       (and (> x min)
+            (< x max))))
 
-(defn select-customer [customer]
-  (let [{:keys [age height]} customer]
-       (and (between age 15 30)
-            (between (first height) 200 270))))
+(defn by-age-and-height [age-range height-range]
+  (fn [customer] (let [{:keys [age height]} customer]
+                  (and (between age age-range)
+                       (between (first height) height-range)))))
 
 
-(filter select-customer customers)
+(filter (by-age-and-height [15 60] [200 300]) customers)
+
+(count (filter (by-age-and-height [15 60] [200 300]) customers))
+
+
 
